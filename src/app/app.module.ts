bd4deaab94d4feeaf4a253299ccd05542cf29edc
Daniel Romero <raiga1234@gmail.com>
2019-08-17 16:38:24 -0500

    automated integration testing, testbed, acces to dom, fixture

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,21 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { MedicosComponent } from './intermediate/spies/medicos.component';
+import { MedicoComponent } from './intermediate2/medico/medico.component';
+import { MedicosService } from './intermediate/spies/medicos.service';
+import { HospitalComponent } from './intermediate2/hospital/hospital.component';
+import { IncrementadorComponent } from './intermediate2/incrementator/incrementador.component';
 
 @NgModule({
-  declarations: [AppComponent, MedicosComponent],
+  declarations: [
+    AppComponent,
+    MedicosComponent,
+    MedicoComponent,
+    HospitalComponent,
+    IncrementadorComponent
+  ],
   imports: [BrowserModule],
-  providers: [],
+  providers: [MedicosService],
   bootstrap: [AppComponent]
 })
 export class AppModule {}
